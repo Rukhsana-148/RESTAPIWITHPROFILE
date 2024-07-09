f16f87b7769c58b026c4245b6f914ada7f1e493d
Rukhsana-148 <rukhsana190148@gmail.com>
2024-07-10 00:45:08 +0600

    Create User model and create user using api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\HTTP\Controllers\APi\ApiController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//register
+Route::post("register", [ApiController::class, "register"]);
+
+
